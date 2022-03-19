d1b82e05a4e3ad9bc90a82b4d49bb093a8d29a01
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-03-19 11:55:05 +0000

    connections

---
+++ db/connections.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'password013',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
+  module.exports = db;
